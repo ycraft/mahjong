2e19ad2c3376374b10737dc9ac817ba78d497f8a
Yuuki Hamada <yuuki3655@gmail.com>
2014-02-16 04:06:58 +0900

    Fix build error

---
+++ src/HandParser.cpp
@@ -0,0 +1,18 @@
+/*
+ * HandParser.cpp
+ *
+ *  Created on: 2014/02/16
+ *      Author: yuuki
+ */
+
+#include <src/HandParser.h>
+
+HandParser::HandParser() {
+  // TODO Auto-generated constructor stub
+
+}
+
+HandParser::~HandParser() {
+  // TODO Auto-generated destructor stub
+}
+
---
+++ src/HandParser.h
@@ -0,0 +1,17 @@
+/*
+ * HandParser.h
+ *
+ *  Created on: 2014/02/16
+ *      Author: yuuki
+ */
+
+#ifndef HANDPARSER_H_
+#define HANDPARSER_H_
+
+class HandParser {
+ public:
+  HandParser();
+  virtual ~HandParser();
+};
+
+#endif /* HANDPARSER_H_ */
